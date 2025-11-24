cd02a5dea9363988a521044b6fc81c503098e37b
Ariyuda11 <ariyudawidnyana15@gmail.com>
2025-11-24 14:13:25 +0700

    fix

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost"; // atau Azure host jika di cloud
-$username = "root"; // atau username Azure
-$password = ""; // atau password Azure
+$host = "tgsmandiri.mysql.database.azure.com"; // atau Azure host jika di cloud
+$username = "ariyuda"; // atau username Azure
+$password = "@admin1511"; // atau password Azure
 $database = "logistik_db"; // <- harus sama persis
 
 $conn = mysqli_init();
